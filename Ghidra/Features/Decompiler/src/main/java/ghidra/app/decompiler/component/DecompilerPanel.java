62db8b33e0c6460716c9d6fbacaf1ac59f0b1335
Gr-i-niy <grigoriyklopov50@gmail.com>
2024-12-05 14:44:36 +0300

    Fixed collapsing inner blocks

--- Ghidra/Features/Decompiler/src/main/java/ghidra/app/decompiler/component/DecompilerPanel.java
+++ Ghidra/Features/Decompiler/src/main/java/ghidra/app/decompiler/component/DecompilerPanel.java
@@ -798,6 +798,7 @@ public class DecompilerPanel extends JPanel implements FieldMouseListener, Field
 				return;
 			}
 
+			boolean isCollapsed = isBlockCollapsed(openingBrace);
 			List<ClangNode> list = new ArrayList<>();
 			openingBrace.Parent().flatten(list);
 
@@ -809,7 +810,7 @@ public class DecompilerPanel extends JPanel implements FieldMouseListener, Field
 						inSection = (!token.equals(closingBrace));
 					}
 					if (inSection) {
-						token.setCollapsedToken(!token.getCollapsedToken());
+						token.setCollapsedToken(!isCollapsed);
 					}
 				}
 				else if ((token instanceof ClangSyntaxToken)) {
